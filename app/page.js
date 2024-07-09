29afc8a1092907e2914b5ebeecae9519465f8525
FarmerSamuel-FS <sfarmer1@student.fullsail.edu>
2024-07-09 15:27:21 -0400

    Intial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Static Next.js Site!</h1>
+      <p>
+        This is a unique edit to demonstrate my ability to update the source
+        code.
+      </p>
+    </div>
+  );
+}
